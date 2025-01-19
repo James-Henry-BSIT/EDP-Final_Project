48b657ce36534d74137664dd7f0f0fb6c1552bf0
James Henry BSIT 3-B <125195068+James-Henry-BSIT@users.noreply.github.com>
2025-01-20 07:12:53 +0800

    Main Files
    Main Files

---
+++ Landing.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EDP_Final_Project
+{
+    /// <summary>
+    /// Interaction logic for Landing.xaml
+    /// </summary>
+    public partial class Landing : Page
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        //MENU SELECT HOVER FUNCTIONS
+        private void HomeLabel_Hover(object sender, MouseEventArgs e)
+        {
+                Storyboard moveToHome = (Storyboard)MenuSelection.FindResource("MoveToHome");
+                moveToHome.Begin();
+
+                Storyboard expandHeight = (Storyboard)MenuSelection.FindResource("ExpandHoverMenuHeight");
+                expandHeight.Begin();
+        }
+
+        private void UpdatesLabel_Hover(object sender, MouseEventArgs e)
+        {
+            Storyboard moveToUpdates = (Storyboard)MenuSelection.FindResource("MoveToUpdates");
+            moveToUpdates.Begin();
+
+            Storyboard expandHeight = (Storyboard)MenuSelection.FindResource("ExpandHoverMenuHeight");
+            expandHeight.Begin();
+        }
+
+        private void CommissionLabel_Hover(object sender, MouseEventArgs e)
+        {
+            Storyboard moveToCommissions = (Storyboard)MenuSelection.FindResource("MoveToCommissions");
+            moveToCommissions.Begin();
+
+            Storyboard expandHeight = (Storyboard)MenuSelection.FindResource("ExpandHoverMenuHeight");
+            expandHeight.Begin();
+        }
+
+        private void NotificationsLabel_Hover(object sender, MouseEventArgs e)
+        {
+            Storyboard moveToNotifications = (Storyboard)MenuSelection.FindResource("MoveToNotifications");
+            moveToNotifications.Begin();
+
+            Storyboard expandHeight = (Storyboard)MenuSelection.FindResource("ExpandHoverMenuHeight");
+            expandHeight.Begin();
+        }
+
+        private void SocialsLabel_Hover(object sender, MouseEventArgs e)
+        {
+            Storyboard moveToSocials = (Storyboard)MenuSelection.FindResource("MoveToSocials");
+            moveToSocials.Begin();
+
+            Storyboard expandHeight = (Storyboard)MenuSelection.FindResource("ExpandHoverMenuHeight");
+            expandHeight.Begin();
+        }
+        //MENU SELECT HOVER FUNCTIONS
+
+        //MENU SELECT CLICK FUNCTIONS
+
+        private void HomeLabel_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void UpdatesLabel_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void CommissionLabel_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void NotificationsLabel_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void SocialsLabel_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+        //MENU SELECT CLICK FUNCTIONS
+        private void Hover_Retract(object sender, MouseEventArgs e)
+        {
+            Storyboard collapseHeight = (Storyboard)MenuSelection.FindResource("CollapseHoverMenuHeight");
+            collapseHeight.Begin();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace EDP_Final_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainFrame.Navigate(new Landing());
+
+        }
+        //Navigate Page
+        public void NavigateToPage(Page page)
+        {
+            MainFrame.Navigate(page);
+        }
+        //Navigate Page
+        //Custom Header Function
+        private void Header_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        //Custom Header Function
+
+
+        //Minimize Window Function
+        private void Minimize_Function(object sender, MouseButtonEventArgs e)
+        {
+            // Minimize the window
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Minimize_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Change the source to Minimize2.png when hovering
+            Minimize.Source = new BitmapImage(new Uri("/Minimize2.png", UriKind.Relative));
+        }
+        private void Minimize_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // Revert the source back to Minimize.png when hover ends
+            Minimize.Source = new BitmapImage(new Uri("/Minimize.png", UriKind.Relative));
+        }
+        //Minimize Window Function
+
+
+        //Close Window Function
+        private void Close_Function(object sender, MouseButtonEventArgs e)
+        {
+            // Close the window
+            this.Close();
+        }
+        private void Close_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Change the source to Minimize2.png when hovering
+            Close.Source = new BitmapImage(new Uri("/Close2.png", UriKind.Relative));
+        }
+        private void Close_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // Revert the source back to Minimize.png when hover ends
+            Close.Source = new BitmapImage(new Uri("/Close.png", UriKind.Relative));
+        }
+        //Close Window Function
+
+
+        //TextField Placeholder functions
+        //Lose Textfield focus
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Check if the focused element is a TextBox or PasswordBox and the click happened outside of it
+            if ((Keyboard.FocusedElement is TextBox textBox && !textBox.IsMouseOver) ||
+                (Keyboard.FocusedElement is PasswordBox passwordBox && !passwordBox.IsMouseOver))
+            {
+                // Move focus to the parent element (e.g., Grid or Window) to remove focus
+                FocusManager.SetFocusedElement(this, null);
+                Keyboard.ClearFocus();
+            }
+        }
+    }
+
+}
